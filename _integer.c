9006e1d28854ef624f8a73c5123930a18c76ef2d
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-22 10:19:01 -0800

    _integer

---
+++ _integer.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * _integer - print int
+ * @l: list of arguments
+ * Return: no. of printerd char
+ */
+
+int _integer(va_list l)
+{
+	inti;
+
+	i = _number(l);
+	return (i);
+}
+
+/**
+ * _number - print num
+ * @args: argument list
+ * Return: no of argu
+ */
+int _number(va_list args)
+{
+	int n;
+	int d;
+	int i;
+	int num;
+
+	n = va_arg(args, int);
+	div = 1;
+	len = 0;
+
+	if (n < 0)
+	{
+		i += _putchar('-');
+		num = n * -1;
+	}
+	else
+	{
+		num = n;
+	}
+	while (num / d > 9)
+	{
+		d *= 10;
+	}
+	while (d != 0)
+	{
+		i += _putchar('0' + num / d);
+		num %= d;
+		d /= 10;
+	}
+	return (i);
+}

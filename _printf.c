3f83a96579c4acd1cdcc1cf6e05001c2b6714c59
Dawit Ayalew Chekol <dawitac@gmail.com>
2022-11-22 09:05:24 -0800

    printf function

--- _printf.c
+++ _printf.c
@@ -2,9 +2,28 @@
 /**
  * _printf - print formated text
  * format: the formater characterr
- * Return: 0
+ * Return: no.of prited char
  */
 
 int _printf(const char *format, ...)
 {
+	int i;
+	
+	convert_special_char list[] = { 
+		{"c", _char}, {"i", _integer}, {"d", _integer}, {"s", _string}, {"u", _unassigned},
+		{"b", _binary}, {"x", _hexdec}, {"o", _octal}, {"p", _pointer}, {"r", _reverse},
+		{"R", _rot13}, {"%", _persent}, {NULL, NULL},};
+
+	va_list arg_list;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	else
+	{
+		va_start(arg_list, format);
+		i = format_reciver(format, list, arg_list);
+		va_end(arg_list);
+		return (i);
 }
